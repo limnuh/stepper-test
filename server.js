ef8b734da484f4073c4c5d0d8253a8e45ba34983
limnuh <barna.tamas@gmail.com>
2019-02-17 19:35:16 +0100

    basic stepper test is completed

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // Transpile all code following this line with babel and use 'env' (aka ES6) preset.
+require("babel-core/register");
+require("babel-polyfill");
 require('babel-register')({
-    presets: [ 'env' ]
+    presets: [ 'env', "es2015", "stage-0" ]
 })
 
 // Import the rest of our application.
--- stepper.js
+++ stepper.js
@@ -1,1 +1,25 @@
-console.log('stepper')
+import { Gpio } from 'onoff';
+
+const enablePin = new Gpio(14, 'out');
+const dirPin = new Gpio(15, 'out');
+const stepPin = new Gpio(18, 'out');
+const stepLength = 2;
+const waitLength = 5;
+
+let stepCount = 1000;
+const sleep = (timeout) => new Promise((resolve) => setTimeout(resolve, timeout));
+
+const stepSome = async (dir, count, next) => {
+  enablePin.writeSync(0);
+  dirPin.writeSync(dir);
+  for (let i = 0; i < count; i++) {
+    //console.log(new Date, 'set dir pin + step pin', i, dir)
+    stepPin.writeSync(1);
+    await sleep(stepLength);
+    stepPin.writeSync(0);
+    await sleep(waitLength);
+  }
+  enablePin.writeSync(1);
+}
+
+stepSome(true, stepCount)
