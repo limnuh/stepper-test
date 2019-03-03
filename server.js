63ef7627794d1e7f96b697050d52655dc8190b1c
tbarna-jay <tamas.barna@jaystack.com>
2019-03-03 22:24:06 +0100

    add servo code

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ require('babel-polyfill');
 if (process.env.LED) return require('./src/led.js');
 if (process.env.STEPPER) return require('./src/stepper.js');
 if (process.env.CNC) return require('./src/cnc.js');
+if (process.env.SERVO) return require('./src/servo.js');
 
 const cnc = require('./src/server.js')
